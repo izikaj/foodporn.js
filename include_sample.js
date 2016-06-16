79e4d6baa12ac0383163f918295a96038dbc47d3
AJ Izik <AJ Izik>
2016-06-17 00:30:42 +0300

    add include sample

---
+++ include_sample.js
@@ -0,0 +1,7 @@
+; // just load script from github
+(function(document) {
+	var s = document.createElement('script');
+	s.setAttribute('type', 'text/javascript');
+	s.setAttribute('src', 'https://izikaj.github.io/foodporn.js/out/main.js');
+	document.body.appendChild(s);
+})(document);
